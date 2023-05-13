83f2a15780b74db61d1242505405063cda909b0d
Vitor e Gabriella <vitorsouzalibrelonrestini@gmail.com>
2023-05-12 22:09:35 -0300

    listaUm completa, listaDois incompleta, menu inconpleto

--- listaUm.py
+++ listaUm.py
@@ -1,50 +1,47 @@
-from listaUm import *
+v = [34, 6, 57, 3, 45, -10, 5]
 
-resp = 's'
-while resp == 's' or resp == 'S':
-    print("----- MENU -----")
-    print("\n NÍVEL 1: \n 1- Primeiro elemento \n 2- Negativo \n 3- Soma \n 4- Média \n 5- Média \n 6- Pri e ult \n 7- Índice par \n 8- Ordenar \n 9- Exista")
-    nivel = int(input("Escolha o nível:"))
-    while nivel != 1 and nivel != 2:
-        print("Erro!")
-        nivel = int(input("Escolha o nível:"))
-    else:
-        if nivel == 1:
-            esc = int(input("Escolha o exercício:"))
-            while esc != 1 and esc != 2 and esc != 3 and esc != 4 and esc != 5 and esc !=6 and esc != 7 and esc != 8 and esc != 9:
-                print("Erro!")
-                esc = int(input("Escolha o exercício:"))
-            else:
-                if esc == 1:
-                    print("Mostrará o primeiro elemento do vetor v = [34, 6, 57, 3, 45, -10, 5]:")
-                    print(primeiro_elem(v))
-                elif esc == 2:
-                    print("Mostrará os valores negativos do vetor v = [34, 6, 57, 3, 45, -10, 5]:")
-                    negativo_elem(v)
-                elif esc == 3:
-                    print("Mostrará a soma dos valores do vetor v = [34, 6, 57, 3, 45, -10, 5]:")
-                    print(soma_elem(v))
-                elif esc == 4:
-                    print("Mostrará a média do vetor v = [34, 6, 57, 3, 45, -10, 5]:")
-                    print(media_elem(v))
-                elif esc == 5:
-                    print("Mostrará os valores ímpares do vetor v = [34, 6, 57, 3, 45, -10, 5]:")
-                    impar_elem(v)
-                elif esc == 6:
-                    print("Mostrará o primeiro e último elemento do vetor v = [34, 6, 57, 3, 45, -10, 5]:")
-                    prim_ulti_elem(v)
-                elif esc == 7:
-                    print("Mostrará os índices pares do vetor v = [34, 6, 57, 3, 45, -10, 5]:")
-                    indice_par_elem(v)
-                elif esc == 8:
-                    print("Mostrará os elemento ordenados do vetor v = [34, 6, 57, 3, 45, -10, 5]:")
-                    print(ordenar(v))
-                else:
-                    print("Mostrará seo número passado como parâmetro existe no vetor v = [34, 6, 57, 3, 45, -10, 5]:")
-                    n1 = input("Digite o valor aser passado como parametro: ")
-                    print(exista(v, n1))
-                resp = input("Deseja continuar:")
-                while resp != 'S' and resp != 's' and resp != 'n' and resp != 'N':
-                    print("Erro!")
-                    resp = input("Deseja continuar:")
+def ordenar(vetor: list) -> list:
+    return print(sorted(vetor))
 
+
+def exista(vetor: list, n1: int) -> bool:
+    for i in range(len(vetor)):
+        if n1 == v[i]:
+            return True
+        else:
+            return False
+
+def primeiro_elem(vetor: list) -> int:
+    return v[0]
+
+def negativo_elem(vetor: list) -> None:
+    for i in range(len(vetor)):
+        if v[i] < 0:
+            print(v[i])
+
+def soma_elem(vetor: list) -> int:
+    soma = 0
+    for i in vetor:
+        soma = soma + i
+    return soma
+
+def media_elem(vetor: list) -> float:
+    soma = 0
+    for i in vetor:
+        soma = soma + i
+    return soma / len(v)
+
+def impar_elem(vetor: list) -> None:
+    for i in range(len(vetor)):
+        if (v[i] % 2 != 0):
+            print("ímpar: ", v[i])
+
+def prim_ulti_elem(vetor: list) -> None:
+    ultimo = len(vetor)
+    primeiro = v[0]
+    print(primeiro, v[ultimo - 1])
+
+def indice_par_elem(vetor: list) -> None:
+    for i in range(len(vetor)):
+        if i % 2 == 0:
+            print(v[i])
